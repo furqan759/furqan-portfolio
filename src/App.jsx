c61b1fc8ee9b53fc3033dead159f7d45c55fd087
furqan759 <muhammadfurqan619@gmail.com>
2025-03-12 15:55:23 +0500

    hello world animation completed | navbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { LoadingScreen } from './components/loadingScreen'
+import { useState } from "react";
+import "./index.css";
+import { LoadingScreen } from "./components/loadingScreen";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <>
-      <LoadingScreen></LoadingScreen>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
