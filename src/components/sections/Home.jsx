64d6ae4d15b3081ea5558edeff04947cd1e5feff
furqan759 <muhammadfurqan619@gmail.com>
2025-04-09 16:29:21 +0500

    about page completed

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
       className="min-h-screen relative flex items-center justify-center"
     >
       <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
           Hi, I'm Furqan Khan
         </h1>
         <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
