273d3aa4f221536840969990133ce1296e5144bc
furqan759 <muhammadfurqan619@gmail.com>
2025-04-09 16:09:36 +0500

    home page completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { LoadingScreen } from "./components/loadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -18,6 +19,7 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
